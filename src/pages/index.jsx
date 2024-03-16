5352ca0eb6c8eea15f7b03a8ec99390964d7bf3a
suuzanemoura <suuzanemoura@gmail.com>
2024-03-16 20:53:27 -0300

    :sparkles: feat: criação do componente hero

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import HeroSection from "../components/Sections/Hero";
 
 export default function IndexPage() {
   return (
     <>
       <Header />
+      <main>
+        <HeroSection />
+      </main>
     </>
   );
 }
